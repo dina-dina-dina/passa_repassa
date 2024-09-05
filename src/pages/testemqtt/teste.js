71c180c081c7e628b626da138101def31846e91e
André Silva <Andr3-silva>
2024-09-05 18:50:41 -0300

    Mqtt funcionando

--- src/pages/testemqtt/teste.js
+++ src/pages/testemqtt/teste.js
@@ -1,24 +1,28 @@
 import mqtt from "mqtt"
-const client = mqtt.connect("mqtt://test.mosquitto.org");
+const client = mqtt.connect("mqtt://127.0.0.1:8080");
 
 client.on("connect", () => {
+  console.log("Entra aqui?")
   client.subscribe("presence", (err) => {
     if (!err) {
       client.publish("presence", "Hello mqtt");
-    }
+    } else {console.log("FOI")}
   });
 });
 
-client.on("message", (topic, message) => {
-  // message is Buffer
-  console.log(message.toString());
-  client.end();
-});
+// client.on("message", (topic, message) => {
+//   console.log("Entra aqui?")
+//   // message is Buffer
+//   console.log(message.toString());
+//   client.end();
+// });
 
 function mqttTeste(){
   return(
     <div></div>
 )
 }
+
+
 
 export default mqttTeste
